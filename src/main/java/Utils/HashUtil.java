ca2ad0ce266cfeee7372782c27d5745fb38de7af
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-01-13 01:18:28 +0700

    create class hash MD5

---
+++ src/main/java/Utils/HashUtil.java
@@ -0,0 +1,31 @@
+package Utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashUtil {
+
+    public static String md5(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : messageDigest) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(HashUtil.md5("123456"));
+    }
+}
