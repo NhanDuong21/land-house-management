339b46d67925c9ebdd683edb64cd5b1038268406
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-01-13 01:17:44 +0700

    HashPass & Remove data not uses

--- src/main/java/DALs/AccountDAO.java
+++ src/main/java/DALs/AccountDAO.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 
 import Models.Account;
 import Utils.DBContext;
+import Utils.HashUtil;
 
 /**
  * Author: Duong Thien Nhan - CE190741 Created on: 2026-01-12
@@ -20,23 +21,21 @@ public class AccountDAO extends DBContext {
         """;
         try (PreparedStatement ps = connection.prepareStatement(sql)) {
             ps.setString(1, username);
-            ps.setString(2, password);
+            ps.setString(2, HashUtil.md5(password));
             try (ResultSet rs = ps.executeQuery()) {
-                if (rs.next()) {
+                if (rs.next() == true) { //check db co null ko
                     Account acc = new Account();
                     acc.setAccountId(rs.getInt("account_id"));
                     acc.setUsername(rs.getString("username"));
-                    acc.setPassword(rs.getString("password"));
-                    acc.setEmail(rs.getString("email"));
-                    acc.setAvatar(rs.getString("avatar"));
                     acc.setRole(rs.getString("role"));
                     acc.setStatus(rs.getString("status"));
                     return acc;
                 }
             }
         } catch (SQLException e) {
-            System.out.println("getById ERROR: " + e.getMessage());
+            System.out.println("LOGIN ERROR: " + e.getMessage());
         }
         return null;
     }
+
 }
