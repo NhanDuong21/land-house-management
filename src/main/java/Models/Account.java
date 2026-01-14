029e33bf6a40d7730d1a9e678e9014de8a354fe5
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-01-15 02:57:34 +0700

    delete full models

--- src/main/java/Models/Account.java
+++ src/main/java/Models/Account.java
@@ -1,86 +1,17 @@
 package Models;
 
+import java.time.LocalDateTime;
+
 /**
  * Author: Duong Thien Nhan - CE190741 Created on: 2026-01-12
  */
 public class Account {
 
-    private int accountId;
+    private Integer accountId;
     private String username;
-    private String password;
+    private String passwordHash;
     private String email;
-    private String avatar;
-    private String role;
-    private String status;
-
-    public Account() {
-    }
-
-    public Account(int accountId, String username, String password, String email, String avatar, String role,
-            String status) {
-        this.accountId = accountId;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.avatar = avatar;
-        this.role = role;
-        this.status = status;
-    }
-
-    public int getAccountId() {
-        return accountId;
-    }
-
-    public void setAccountId(int accountId) {
-        this.accountId = accountId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAvatar() {
-        return avatar;
-    }
-
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
+    private int status; // 0=inactive, 1=active, 2=locked
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt; 
 }
