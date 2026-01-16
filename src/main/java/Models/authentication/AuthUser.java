55bdfb7c903a3195ab4560205bcf980dfb8bf119
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-01-16 15:42:19 +0700

    add authentication help setting role staff & tenant

--- src/main/java/Models/authentication/AuthUser.java
+++ src/main/java/Models/authentication/AuthUser.java
@@ -1,11 +1,48 @@
 package Models.authentication;
 
 /**
- * Description
+ * Description: save infor login in session
  *
  * @author Duong Thien Nhan - CE190741
  * @since 2026-01-16
  */
 public class AuthUser {
+
+    private int id;
+    private String fullName;
+    private String role;
+
+    public AuthUser() {
+    }
+
+    public AuthUser(int id, String fullName, String role) {
+        this.id = id;
+        this.fullName = fullName;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 
 }
