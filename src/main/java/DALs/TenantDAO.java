cbd4a4eaa81fec419a205c1338655233f36a92f7
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-01-16 16:47:54 +0700

    create method login for tenant

---
+++ src/main/java/DALs/TenantDAO.java
@@ -0,0 +1,43 @@
+package DALs;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import Models.authentication.AuthUser;
+import Utils.DBContext;
+import Utils.HashUtil;
+
+/**
+ * Description
+ *
+ * @author Duong Thien Nhan - CE190741
+ * @since 2026-01-16
+ */
+public class TenantDAO extends DBContext {
+
+    public AuthUser login(String username, String password) {
+        String sql = "SELECT        tenant_id, full_name, status "
+                + "FROM TENANTS WHERE username = ? AND password_hash = ?";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, username);
+            ps.setString(2, HashUtil.md5(password));
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next() == true) {
+                    byte status = rs.getByte("status");
+                    if (status != 1) {
+                        return null;
+                    }
+                    AuthUser authUser = new AuthUser();
+                    authUser.setId(rs.getInt("tenant_id"));
+                    authUser.setFullName(rs.getString("full_name"));
+                    authUser.setRole("TENANT"); // vì chỉ có 1 entity tenant duy nhất
+                    return authUser;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
