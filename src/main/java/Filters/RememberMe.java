2cc16c8818caa7070664d830c52c72a065efa163
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-01-17 15:21:02 +0700

    succes logic remember tenant  & staff

--- src/main/java/Filters/RememberMe.java
+++ src/main/java/Filters/RememberMe.java
@@ -2,11 +2,17 @@ package Filters;
 
 import java.io.IOException;
 
+import DALs.StaffDAO;
+import DALs.TenantDAO;
+import Models.authentication.AuthUser;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 public class RememberMe implements Filter {
 
@@ -14,36 +20,55 @@ public class RememberMe implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException {
 
-        // HttpServletRequest request = (HttpServletRequest) req;
-        // String uri = request.getRequestURI();
-        // //BỎ QUA FILTER VỚI CÁC URL PUBLIC
-        // if (uri.endsWith("/home")
-        //         || uri.endsWith("/login")
-        //         || uri.endsWith("/register")
-        //         || uri.contains("/assets/")) {
-        //     chain.doFilter(req, res);
-        //     return;
-        // }
-        // HttpSession session = request.getSession(false);
-        // // Nếu chưa login
-        // if (session == null || session.getAttribute("account") == null) {
-        //     Cookie[] cookies = request.getCookies(); // client send all cookie of domain
-        //     if (cookies != null) {
-        //         for (Cookie c : cookies) {
-        //             if ("Remember_me".equals(c.getName())) {
-        //                 String token = c.getValue(); // get token(chuỗi random (UUID)) từ cookie
-        //                 AccountDAO dao = new AccountDAO();
-        //                 Account acc = dao.findByRememberToken(token); // check sự tồn tại của cookie
-        //                 if (acc != null) { // cookie tồn tại => create new session vì trước đó user chưa login nên cần session mới để lưu account
-        //                     HttpSession newSession = request.getSession(true);
-        //                     newSession.setAttribute("account", acc);
-        //                     newSession.setAttribute("role", acc.getRole());
-        //                 }
-        //                 break;
-        //             }
-        //         }
-        //     }
-        // }
-        // chain.doFilter(req, res); // allow request đi tiếp
+        HttpServletRequest request = (HttpServletRequest) req;
+        String uri = request.getRequestURI();
+
+        // BỎ QUA FILTER VỚI CÁC URL PUBLIC
+        if (uri.endsWith("/login")
+                || uri.endsWith("/register")
+                || uri.contains("/assets/")) {
+            chain.doFilter(req, res);
+            return;
+        }
+
+        HttpSession session = request.getSession(false);
+
+        // Nếu chưa login thì check cookie remember
+        if (session == null || session.getAttribute("auth") == null) {
+
+            String token = null;
+
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null) {
+                for (Cookie c : cookies) {
+                    if ("Remember_me".equals(c.getName())) {
+                        token = c.getValue();
+                        break;
+                    }
+                }
+            }
+
+            if (token != null && !token.isBlank()) {
+
+                // thử tìm trong staff trước
+                StaffDAO sdao = new StaffDAO();
+                AuthUser authUser = sdao.findByRememberToken(token);
+
+                // nếu staff null -> thử tenant
+                if (authUser == null) {
+                    TenantDAO tdao = new TenantDAO();
+                    authUser = tdao.findByRememberToken(token);
+                }
+
+                // nếu tìm thấy -> tạo session mới
+                if (authUser != null) {
+                    HttpSession newSession = request.getSession(true);
+                    newSession.setAttribute("auth", authUser);
+                    newSession.setAttribute("role", authUser.getRole());
+                }
+            }
+        }
+
+        chain.doFilter(req, res);
     }
 }
