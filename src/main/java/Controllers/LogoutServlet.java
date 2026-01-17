8d19411a6a8df25046b1bd51816a1bc1fc5e1e2c
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-01-17 15:17:24 +0700

    done Logout logic

--- src/main/java/Controllers/LogoutServlet.java
+++ src/main/java/Controllers/LogoutServlet.java
@@ -6,10 +6,15 @@ package Controllers;
 
 import java.io.IOException;
 
+import DALs.StaffDAO;
+import DALs.TenantDAO;
+import Models.authentication.AuthUser;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 /**
  *
@@ -21,21 +26,26 @@ public class LogoutServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        // HttpSession session = request.getSession(false); // get sesssion hiện tại (false)
-        // if (session != null) {
-        //     Account acc = (Account) session.getAttribute("account"); //get account đang login
-        //     if (acc != null) {
-        //         AccountDAO dao = new AccountDAO();
-        //         dao.clearRememberToken(acc.getAccountId());
-        //     }
-        //     session.invalidate(); // delete all session data ( lúc này "account" và "role" biến mất ) => user chính thức đã logout
-        // }
-        // // create cookie rỗng để Delete cookie Remember_me
-        // Cookie c = new Cookie("Remember_me", "");
-        // c.setMaxAge(0); // yêu cầu client delete cookie
-        // c.setPath(request.getContextPath()); //path giống lúc tạo
-        // response.addCookie(c); //send request delete về client
-        // response.sendRedirect("login");
-        // response.sendRedirect(request.getContextPath() + "/home");
+        HttpSession session = request.getSession(false); // get sesssion hiện tại (false)
+        if (session != null) {
+            AuthUser authUser = (AuthUser) session.getAttribute("auth"); //get account đang login
+            if (authUser != null) {
+                if ("ADMIN".equalsIgnoreCase(authUser.getRole()) || "MANAGER".equalsIgnoreCase(authUser.getRole())) {
+                    StaffDAO sdao = new StaffDAO();
+                    sdao.clearRememberToken(authUser.getId());
+                } else if ("TENANT".equalsIgnoreCase(authUser.getRole())) {
+                    TenantDAO tdao = new TenantDAO();
+                    tdao.clearRememberToken(authUser.getId());
+                }
+            }
+            session.invalidate(); // delete all session data ( lúc này "account" và "role" biến mất ) => user chính thức đã logout
+        }
+        // create cookie rỗng để Delete cookie Remember_me
+        Cookie c = new Cookie("Remember_me", "");
+        c.setMaxAge(0); // yêu cầu client delete cookie
+        c.setPath(request.getContextPath()); //path giống lúc tạo
+        response.addCookie(c); //send request delete về client
+
+        response.sendRedirect(request.getContextPath() + "/home");
     }
 }
