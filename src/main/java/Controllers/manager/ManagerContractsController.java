59f6237cebc36977065449965d16d59645a1f508
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-02-11 16:07:25 +0700

    manage contracts dashboard

---
+++ src/main/java/Controllers/manager/ManagerContractsController.java
@@ -0,0 +1,19 @@
+package Controllers.manager;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/manager/contracts")
+public class ManagerContractsController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/views/manager/contracts.jsp").forward(request, response);
+    }
+}
