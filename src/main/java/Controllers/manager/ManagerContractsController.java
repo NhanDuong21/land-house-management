beb9cfb5b87e24f1696d71196a37a934fd428eca
NhanDTCE190741 <nhandt.ce190741@gmail.com>
2026-02-12 00:25:07 +0700

    dissplayed lost contract dto

--- src/main/java/Controllers/manager/ManagerContractsController.java
+++ src/main/java/Controllers/manager/ManagerContractsController.java
@@ -1,7 +1,10 @@
 package Controllers.manager;
 
 import java.io.IOException;
+import java.util.List;
 
+import DALs.contract.ContractDAO;
+import Models.dto.ManagerContractRowDTO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,9 +14,15 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/manager/contracts")
 public class ManagerContractsController extends HttpServlet {
 
+    private final ContractDAO contractDAO = new ContractDAO();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        List<ManagerContractRowDTO> list = contractDAO.getManagerContracts();
+        request.setAttribute("contracts", list);
+
         request.getRequestDispatcher("/views/manager/contracts.jsp").forward(request, response);
     }
 }
