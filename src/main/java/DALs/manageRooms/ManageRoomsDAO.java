5786703149011ba3ac68813e90f7ea4f79ea01e4
Khangthce <Khangthce190729@gmail.com>
2026-02-25 17:17:35 +0700

    Update FetchAllRoom and create countRoom method

--- src/main/java/DALs/manageRooms/ManageRoomsDAO.java
+++ src/main/java/DALs/manageRooms/ManageRoomsDAO.java
@@ -6,6 +6,7 @@ package DALs.manageRooms;
 
 import Models.entity.Room;
 import Utils.database.DBContext;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,23 +19,26 @@ import java.util.List;
  */
 public class ManageRoomsDAO extends DBContext {
 
-    public List<Room> fetchAllRoom() {
+    public List<Room> fetchAllRoom(int pageIndex, int pageSize) {
+        List<Room> list = new ArrayList<>();
+
         String sql
-                = "SELECT r.*, b.blockName "
+                = "SELECT r.*, b.block_name "
                 + "FROM Room r "
-                + "JOIN Block b ON r.blockId = b.blockId "
-                + "ORDER BY r.roomId "
+                + "JOIN Block b ON r.block_id = b.block_id "
+                + "ORDER BY r.room_id "
                 + "OFFSET ? ROWS FETCH NEXT ? ROWS ONLY";
 
-        List<Room> Rooms = new ArrayList<>();
-        try {
-            PreparedStatement ps = connection.prepareStatement(sql);
-//            int offset = (pageIndex - 1) * pageSize;
-//            ps.setInt(1, offset);
-//            ps.setInt(2, pageSize);
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            ps.setInt(1, (pageIndex - 1) * pageSize);
+            ps.setInt(2, pageSize);
+
             ResultSet rs = ps.executeQuery();
+
             while (rs.next()) {
                 Room p = new Room();
+
                 p.setRoomId(rs.getInt("room_id"));
                 p.setBlockName(rs.getString("block_name"));
                 p.setRoomNumber(rs.getString("room_number"));
@@ -45,12 +49,26 @@ public class ManageRoomsDAO extends DBContext {
                 p.setMezzanine(rs.getBoolean("is_mezzanine"));
                 p.setAirConditioning(rs.getBoolean("has_air_conditioning"));
                 p.setStatus(rs.getString("status"));
-                Rooms.add(p);
+
+                list.add(p);
             }
-        } catch (SQLException ex) {
-            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return Rooms;
+        return list;
     }
-    
+
+    public int countRoom() {
+        String sql = "SELECT COUNT(*) FROM Room";
+
+        try (Connection conn = getConnection(); PreparedStatement ps = conn.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
+
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
 }
