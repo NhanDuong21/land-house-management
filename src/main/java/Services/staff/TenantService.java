fb647d7097b0a5f902d7f98a5954428881f1a6a9
ThanhDH <thanhdh.ce191422@gmail.com>
2026-02-27 02:53:36 +0700

    ThanhDH_in the TenantService.java file, add a function ...

--- src/main/java/Services/staff/TenantService.java
+++ src/main/java/Services/staff/TenantService.java
@@ -4,10 +4,19 @@
  */
 package Services.staff;
 
+
+import DALs.auth.StaffDAO;
+import Models.entity.Tenant;
+import java.util.List;
+
 /**
  *
  * @author ADMIN
  */
 public class TenantService {
-    
+        private StaffDAO StaffDAO = new StaffDAO();
+
+    public List<Tenant> getAllTenants() {
+        return StaffDAO.getAllTenants();
+    }
 }
