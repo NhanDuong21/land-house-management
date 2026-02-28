d6f3511f2176d50e002523783fd27cc4c0b52e12
Y Bui <buinhuy312@gmail.com>
2026-02-28 11:34:42 +0700

    YBN_Utilities DAO

--- src/main/java/DALs/utilities/utilitiesDAO.java
+++ src/main/java/DALs/utilities/utilitiesDAO.java
@@ -6,6 +6,7 @@ package DALs.utilities;
 
 import Models.entity.Utility;
 import Utils.database.DBContext;
+import java.math.BigDecimal;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -43,8 +44,85 @@ public class utilitiesDAO extends DBContext {
         return listUntilities;
     }
 
-    
-    
+    public boolean addUtility(String utilityName, BigDecimal standardPrice, String unit) {
+        String sql = "insert into utility(utility_name, standard_price, unit, is_active, status, created_at, updated_at) \n"
+                + "values(?, ?, ?, 1, 'ACTIVE', GETDATE(), GETDATE())";
+        //mình add tỏng sql là có 5 dữ liệu nên là 5 dấu hỏi á
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, utilityName);
+            ps.setBigDecimal(2, standardPrice);
+            ps.setString(3, unit);
+
+            //thay đổi dữ liệu nên xài executeUpdate()
+            //còn executeQuery() nó k có lm thay đổi 
+            int row = ps.executeUpdate();
+            if (row > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public Utility getUtilityById(int id) {
+        String sql = "SELECT * FROM UTILITY WHERE utility_id = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                Utility u = new Utility();
+                u.setUtilityId(rs.getInt("utility_id"));
+                u.setUtilityName(rs.getString("utility_name"));
+                u.setUnit(rs.getString("unit"));
+                u.setStandardPrice(rs.getBigDecimal("standard_price"));
+                u.setActive(rs.getBoolean("is_active"));
+                u.setStatus(rs.getString("status"));
+                return u;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Boolean deleteUtilities(int id) {
+        String sql = "DELETE FROM UTILITY WHERE utility_id = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setInt(1, id);
+            //trả về kiểu int kiểu số lượng dòng mình xóa hay mình thêm
+            //update mà ra 0 là k có update đc dòng nào
+            int num = ps.executeUpdate();
+            if (num > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean updateUtilities(int id, BigDecimal price) {
+        String sql = "UPDATE UTILITY SET standard_price = ?, updated_at = GETDATE() WHERE utility_id = ?";
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setBigDecimal(1, price);
+            ps.setInt(2, id);
+            int num = ps.executeUpdate();
+            return num > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         utilitiesDAO ud = new utilitiesDAO();
         List<Utility> list = ud.getManagerUntilities();
