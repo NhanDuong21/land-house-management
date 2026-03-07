de505cd82677b526dd9c9c20abd8189540c19427
Khangthce <Khangthce190729@gmail.com>
2026-03-08 04:14:25 +0700

    Khang: Create a Maintenance Request Controller class to handle the display of the tenant request list and process the functionality through the page.

---
+++ src/main/java/Controllers/manager/MaintenanceRequestController.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controllers.manager;
+
+import java.io.IOException;
+import java.util.List;
+
+import DALs.maintenanceRequest.MaintenanceRequestDAO;
+import Models.dto.MaintenanceRequestDTO;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author truon
+ */
+public class MaintenanceRequestController extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        MaintenanceRequestDAO dao = new MaintenanceRequestDAO();
+        String action = request.getParameter("action");
+        if ("edit".equals(action)) {
+            showMaintenanceDetail(request, response, dao);
+            return;
+        }
+        int pageIndex = 1;
+        int pageSize = 10;
+        String page = request.getParameter("page");
+
+        if (page != null) {
+            try {
+                pageIndex = Integer.parseInt(page);
+            } catch (NumberFormatException e) {
+                pageIndex = 1;
+            }
+        }
+
+        int totalRequest = dao.countRequest();
+        int totalPage = (int) Math.ceil((double) totalRequest / pageSize);
+
+        if (totalPage == 0) {
+            totalPage = 1;
+        }
+
+        if (pageIndex < 1) {
+            pageIndex = 1;
+        }
+
+        if (pageIndex > totalPage) {
+            pageIndex = totalPage;
+        }
+        List<MaintenanceRequestDTO> list = dao.getAllRequests();
+        request.setAttribute("requests", list);
+        request.setAttribute("totalRequest", totalRequest);
+        request.setAttribute("pageIndex", pageIndex);
+        request.setAttribute("totalPage", totalPage);
+        request.getRequestDispatcher("/views/manager/maintenanceListForManager.jsp")
+                .forward(request, response);
+    }
+
+
+    private void showMaintenanceDetail(HttpServletRequest request, HttpServletResponse response, MaintenanceRequestDAO dao)
+            throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        MaintenanceRequestDTO maintenance = dao.getRequestById(id);
+        request.setAttribute("maintenance", maintenance);
+        request.getRequestDispatcher("/views/manager/editMaintenance.jsp")
+                .forward(request, response);
+    }
+}
